6cb104a0f7ce790c27d781138b1ab3c10c545dfe
Hayden Miedema <haydenmiedema@gmail.com>
2018-10-15 23:47:23 -0400

    add a programming languages route and view

--- HaydenWebsite.Server/Controllers/DataController.cs
+++ HaydenWebsite.Server/Controllers/DataController.cs
@@ -8,11 +8,16 @@ using System.Threading.Tasks;
 namespace HaydenWebsite.Server.Controllers
 {
     [Route("api/[controller]")]
-    public class SampleDataController : Controller
+    public class DataController : Controller
     {
         private static string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private static string[] Languages = new[]
+        {
+            "C#", "Python", "Ruby/RoR", "Java", "AngularJS", "jQuery", "Javascript", "Visual Basic"
         };
 
         [HttpGet("[action]")]
@@ -24,6 +29,17 @@ namespace HaydenWebsite.Server.Controllers
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
+            });
+        }
+        
+        [HttpGet("[action]")]
+        public IEnumerable<ProgrammingLanguage> ProgrammingLanguages()
+        { 
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new ProgrammingLanguage
+            {
+                Name = Languages[rng.Next(Languages.Length)],
+                Rating = rng.Next()
             });
         }
     }
---
+++ HaydenWebsite.Shared/ProgrammingLanguage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HaydenWebsite.Shared
+{
+    public class ProgrammingLanguage
+    {
+        public string Name { get; set; }
+        public int Rating { get; set; }
+    }
+}
