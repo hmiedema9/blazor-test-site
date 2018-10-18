b6ffb937b8ae83b6c8d69cf0b9a6436bbd1c68f6
Hayden Miedema <haydenmiedema@gmail.com>
2018-10-18 14:16:42 -0400

    db access files

---
+++ HaydenWebsite.Server/DataAccess/LanguageContext.cs
@@ -0,0 +1,22 @@
+﻿using HaydenWebsite.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HaydenWebsite.Server.DataAccess
+{
+    public class LanguageContext : DbContext
+    {
+        public virtual DbSet<ProgrammingLanguage> tblLanguage { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=tcp:haydenmiedemaserver.database.windows.net,1433;Initial Catalog=haydenmiedemadb;Persist Security Info=False;User ID=[dbuser];Password=[dbpass];MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+    }
+}
---
+++ HaydenWebsite.Server/DataAccess/LanguageDataAccessLayer.cs
@@ -0,0 +1,85 @@
+﻿using HaydenWebsite.Server.DataAccess;
+using HaydenWebsite.Shared.Models;  
+using Microsoft.EntityFrameworkCore;  
+using System;  
+using System.Collections.Generic;  
+using System.Linq;  
+using System.Threading.Tasks;  
+  
+namespace HaydenWebsite.Server.DataAccess
+{
+    public class LanguageDataAccessLayer
+    {
+        LanguageContext db = new LanguageContext();
+
+        //To Get all Languages details   
+        public IEnumerable<ProgrammingLanguage> GetAllLanguages()
+        {
+            try
+            {
+                return db.tblLanguage.ToList();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //To Add new Language record     
+        public void AddLanguage(ProgrammingLanguage Language)
+        {
+            try
+            {
+                db.tblLanguage.Add(Language);
+                db.SaveChanges();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //To Update the records of a particluar Language    
+        public void UpdateLanguage(ProgrammingLanguage Language)
+        {
+            try
+            {
+                db.Entry(Language).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //Get the details of a particular Language    
+        public ProgrammingLanguage GetLanguageData(int id)
+        {
+            try
+            {
+                ProgrammingLanguage Language = db.tblLanguage.Find(id);
+                return Language;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //To Delete the record of a particular Language    
+        public void DeleteLanguage(int id)
+        {
+            try
+            {
+                ProgrammingLanguage lang = db.tblLanguage.Find(id);
+                db.tblLanguage.Remove(lang);
+                db.SaveChanges();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
